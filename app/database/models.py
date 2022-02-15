f0f066ca53461c3c253ee7938e82be908cb7a9fb
JuanMH <juanmhuerta@gmail.com>
2022-02-15 00:06:13 -0300

    WIP: login

--- app/database/models.py
+++ app/database/models.py
@@ -1,5 +1,17 @@
 from .db import db
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 class Notice(db.Document):
     title = db.StringField(max_length=255, required=True, unique=True)
     content = db.StringField(max_length=255, required=True)
+
+class User(db.Document):
+    email = db.StringField(max_length=255, required=True, unique=True)
+    password = db.StringField(max_length=255, required=True, min_length=6)
+    username = db.StringField(max_length=255, required=True, unique=True)
+
+    def hash_password(self):
+        self.password = generate_password_hash(self.password).decode('utf8')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
--- app/resources/notices.py
+++ app/resources/notices.py
@@ -1,5 +1,6 @@
 from flask import request, Response
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required
 from database.models import Notice
 
 class NoticesApi(Resource):
@@ -7,6 +8,7 @@ class NoticesApi(Resource):
         notices = Notice.objects.all()
         return {'notices': [notice.to_json() for notice in notices]}
 
+    @jwt_required
     def post(self):
         data = request.get_json()
         notice = Notice(**data)
@@ -14,11 +16,13 @@ class NoticesApi(Resource):
         return {'notice': notice.to_json()}
 
 class NoticeApi(Resource):
+    @jwt_required
     def delete(self, id):
         data = request.get_json()
         Notice.objects(id=data['id']).delete()
         return {'message': 'notice deleted'}
     
+    @jwt_required
     def put(self, id):
         data = request.get_json()
         Notice.objects(id=data['id']).update(**data)
